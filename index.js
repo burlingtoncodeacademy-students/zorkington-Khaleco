23eee6898b7e38efa23c03cb86a9835c69e5cc49
Khale Coffran <khalecoffran@gmail.com>
2023-10-03 18:56:55 -0400

    added maze search and fixed spelling

--- index.js
+++ index.js
@@ -53,50 +53,50 @@ var hexagonPedestal = new Pedestal([]);
 // All Location class objects ("Name" , "Description", "Further Description" , ["items"])
 const entrance = new Location("You are at the Entrance", "The entrance to the lich's lair lies ahead of you. Peering in you see a long dark 'hallway'", "The sky is clouded and dark. Above the entrance you see a passage enscribed: Dragons fall and stars die,\nIn the end I shall rise.");
 
-const hallway = new Location("You are in the Hallway", "You find yourself in a large hallway in the shape of an L. There are three exits, a door which is labled 'library', at the end of the hallway a room with a 'mural', and the 'entrance'.", "Looking around you spot strange marks on the floor and a slight draft where there shouldn't be one.");
+const hallway = new Location("You are in the Hallway", "You find yourself in a large hallway in the shape of an L. There are three exits, a door which is labled 'library', at the end of the hallway a room with a 'muralRoom', and the 'entrance'.", "Looking around you spot strange marks on the floor and a slight draft where there shouldn't be one.");
 
-const library = new Location("You are in the Library", "This room is stacked with books all the way to the ceiling. Dust clings to every surface and the air is damp filled with the smell of decaying paper. There is only one exit 'hallway'.", "Looking through the books none catch you eye as useful, execpt one: On The Creation of Phylacterys. When you grab the book a 'passage' opens up to your right. You open the book to find most of its contents rotted. There is but one ledgable passage: Phylacterys are the key to immortalty for any aspiring wizard who cares not for what he must do in order to obtain it, nor what the gerneral populice think of them.");
+const library = new Location("You are in the Library", "This room is stacked with books all the way to the ceiling. Dust clings to every surface and the air is damp filled with the smell of decaying paper. There is only one exit 'hallway'.", "Looking through the books none catch your eye as useful, execpt one: On The Creation of Phylacterys. When you grab the book a 'passage' opens up to your right. You open the book to find most of its contents rotted. There is but one ledgable passage: Phylacterys are the key to immortalty for any aspiring wizard who cares not for what he must do in order to obtain it, nor what the gerneral populice think of them.");
 
-const passage = new Location("You are in the Passageway", "you enter the small dank passage one end leads to an 'alter' the other the 'library'.", "You notice a 'crackedWall' within the passage peering closed you see a room on the other side. If only you had a way to break it.", "", "crackedWall");
+const passage = new Location("You are in the Passageway", "you enter the small dank passage one end leads to an 'alterRoom' the other the 'library'.", "You notice a 'crackedWall' within the passage peering closed you see a room on the other side. If only you had a way to break it.", "", "crackedWall");
 // Upon using the knife to bleed into the bowl, the way to the stairs is revealed
-const alter = new Location("You are in the Alter Room", "A small chapel, rows of seats face a large statue of an unknown figure. Below the figure sits a bowl. Below the bowl there is an insciption: For those who dare to follow, you must give me part of your'self'. There is only one exit 'passage'.", "Looking around the room you find little beyond what was noticed at first glance. Looking closer at the bowl you notice deep red stains and the feint sent of iron.", ["knife"], "self");
+const alterRoom = new Location("You are in the Alter Room", "A small chapel, rows of seats face a large statue of an unknown figure. Below the figure sits a bowl. Below the bowl there is an insciption: For those who dare to follow, you must give me part of your'self'. There is only one exit 'passage'.", "Looking around the room you find little beyond what was noticed at first glance. Looking closer at the bowl you notice deep red stains and the feint sent of iron.", ["knife"], "self");
 // secret room contains first item for secret ending 'evilsEnd'
 const secretRoom = new Location("You are in a Secret Room", "You enter into a small room containing a small alter. on which a sword lies. Looking closer at the alter you see an insciption: When stars die more are born,\nI will be 'evilsEnd' for this I am sworn. There is only one exit 'passage'", "There is nothing else in this room.", ["evilsEnd"]);
 
-const mural = new Location("You are in the Mural Room", "This room is a semi circle. There is a large mural that encompasses the entire curved wall.", "The mural depicts a shinning star flanked by two dragons, a gold one on the left and a black one on the right.");
+const muralRoom = new Location("You are in the Mural Room", "This room is a semi circle. There is a large mural that encompasses the entire curved wall.", "The mural depicts a shinning star flanked by two dragons, a gold one on the left and a black one on the right.");
 
 const stairs = new Location("You are at the top of the stairs", "Large spiraled stairs lead to the 'hallway' and 'grandHall'.", "Looking around you find nothing of intrest.");
 
-const grandHall = new Location("You are in the Grand Hall", "You enter into a grand hall. Torches line walls giving off a dim light. In the center of the room stants four pedestals facing a 'largeDoor'\n   ( )\n( )   ( )\n   ( )\nEach pedestal is marked. The one closest to the 'largeDoor' has a square carved into it 'sqarePedestal'. The one on the left has a triangle 'trianglePedestal'. The one on the right has a circle 'circlePedestal'. The one furthest from the door has 'hexagonPedestal'. There are two doors on either side of the room and the door to the 'stairs'. Each of the doors has a different color white, black, yellow, and red. 'colorDoor'", "The door is inscibed with a passage: Of the four, the brighted shall face the door", "", ["squarePedestal, HexagonPedestal, trianglePedestal, circlePedestal"]);
+const grandHall = new Location("You are in the Grand Hall", "You enter into a grand hall. Torches line walls giving off a dim light. In the center of the room stants four pedestals facing a 'largeDoor'\n   (sqr)\n(tri)   (cir)\n   (hex)\nEach pedestal is marked. The one closest to the 'largeDoor' has a square carved into it 'sqarePedestal'. The one on the left has a triangle 'trianglePedestal'. The one on the right has a circle 'circlePedestal'. The one furthest from the door has 'hexagonPedestal'. There are two doors on either side of the room and the door to the 'stairs'. Each of the doors has a different color white, black, yellow, and red. 'colorDoor'", "The door is inscibed with a passage: Of the four, the brightest shall face the door", "", ["squarePedestal, HexagonPedestal, trianglePedestal, circlePedestal"]);
 
 const whiteDoor = new Location("You are in the White Room", "The room is filled with candles forming a shrine. At the center there lies a closed book bound in simple leather.", "The book has no cover, no title, and no words within. As you flip through the book a sense of deja vu washes over you. Have you been here? done this already?", ["candle"]);
 
 const redDoor = new Location("You are in the Red Room", "Old wood lines the walls and floor of this room. A small fire is lit in a fireplace softly illuminating the room. Old trinkets line the shelves but one stands out, an empty jar labled: What remains.", "Looking through the colloction nothing catches your eye.", ["emptyJar"]);
 
-const yellowDoor = new Location("You are in the Yellow Room", "This room is made out of white marble with gold trim and accents. A large painting hangs on the wall and befor it sit a pedestal.", "The painting is that of a happy family stading on hill basking in the sun together. One figure sits looking longingly at them in the shadows.", ["goldHeart"]);
+const yellowDoor = new Location("You are in the Yellow Room", "This room is made out of white marble with gold trim and accents. A large painting hangs on the wall and before it sit a pedestal.", "The painting is that of a happy family stading on hill basking in the sun together. One figure sits looking longingly at them in the shadows.", ["goldHeart"]);
 
 const blackDoor = new Location("You are in the Black Room", "Sharp jagged metal pierces the floor and walls of this room. One large braizer sits in the center, red flames reaching for the ceiling. Through the filcker of flame you make out the silhouette of a warhammer.", "Taking a closer look aroud you feel an overwhelming sense of hatred flood your mind.", ["blackHammer"]);
 
 const largeDoor = new Location("You are at the Large Door", "You open the large door connecting the 'grandHall' to the 'mazeStart'.", "Looking around you find nothing of intrest.");
 
-const mazeStart = new Location("You are at the Start of the maze", "haveing come from the 'largeDoor' you look around you see three paths before you, 'right1', 'forward1', 'left1'.");
+const mazeStart = new Location("You are at the Start of the maze", "having come from the 'largeDoor' you look around you see three paths before you, 'right1', 'forward1', 'left1'.", "Looking around you find nothing of intrest.");
 
-const right1 = new Location("You are in the maze","You've run into a dead end. The only way to go is 'mazeStart'.");
+const right1 = new Location("You are in the maze","You've run into a dead end. The only way to go is 'mazeStart'.", "Looking around you find nothing of intrest.");
 
-const forward1 = new Location("You are in the maze","You've run into a dead end. The only way to go is 'mazeStart'.");
+const forward1 = new Location("You are in the maze","You've run into a dead end. The only way to go is 'mazeStart'.", "Looking around you find nothing of intrest.");
 
-const left1 = new Location("You are in the maze", "After a couple of turns you find yourself at another crossroads. There is a path to the 'left2', one 'forward2', and back to 'mazeStart'.");
+const left1 = new Location("You are in the maze", "After a couple of turns you find yourself at another crossroads. There is a path to the 'left2', one 'forward2', and back to 'mazeStart'.", "Looking around you find nothing of intrest.");
 
-const left2 = new Location("You are in the maze", "You've run into a dead end. The only way to go is 'left1'.");
+const left2 = new Location("You are in the maze", "You've run into a dead end. The only way to go is 'left1'.", "Looking around you find nothing of intrest.");
 
-const forward2 = new Location("You are in the maze", "The maze continues on for what feels like hours befor you find another crossroads. 'left3', 'forward3', or back to 'left1'.");
+const forward2 = new Location("You are in the maze", "The maze continues on for what feels like hours before you find another crossroads. 'left3', 'forward3', or back to 'left1'.", "Looking around you find nothing of intrest.");
 
-const forward3 = new Location("You are in the maze","You've run into a dead end. The only way to go is 'forward2'.", "You notice a small crack in the wall('smallCrackedWall').", "", "smallCrackedWall");
+const forward3 = new Location("You are in the maze","You've run into a dead end. The only way to go is 'forward2'.", "You notice a small crack in the wall('smallCrackedWall').", "Looking around you find nothing of intrest.", "smallCrackedWall");
 // contains the second half for the secret ending
 // player must use 'evilsEnd' on 'phylactery'
 const secret = new Location("You are in a secret room", "You enter into a small alcove, dry and forgotten. A strange glow eminates from a crystal heart that sits within an exquisite glass jar, a 'phylactery'. The only exit from this room being 'forward3'.", "There is nothing else in this room but dust and cobwebs.");
 
-const left3 = new Location("You are in the maze", "After following this path for awhile you come across an exit, Ruthom's 'sanctum' or you could turn back 'forward2'");
+const left3 = new Location("You are in the maze", "After following this path for awhile you come across an exit, Ruthom's 'sanctum' or you could turn back 'forward2'", "Looking around you find nothing of intrest.");
 
 const sanctum = new Location("You are within Ruthom's Sanctum", "The stench of rot fills the air. Before you lies Ruthom unaware of your presence, or perhaps he simply does not care about your presence, he works away none the less. What he is doing you can only imagine but whatever it is it must be stopped you must 'fight' him. But are you the one to do it? You could turn back 'left3'.", "Alchemical odds and ends and magical items cluter the walls of this room but you don't know what any of them do.");
 
@@ -106,9 +106,9 @@ const locationLookUp = {
   "hallway": hallway,
   "library": library,
   "passage": passage, 
-  "alter": alter,
+  "alterRoom": alterRoom,
   "secretRoom": secretRoom,
-  "mural": mural,
+  "muralRoom": muralRoom,
   "stairs": stairs,
   "grandHall": grandHall,
   "whiteDoor": whiteDoor,
@@ -130,12 +130,12 @@ const locationLookUp = {
 
 const locationStates = {
   entrance: ["hallway"],
-  hallway: ["entrance", "library", "mural", "stairs"],
+  hallway: ["entrance", "library", "muralRoom", "stairs"],
   library: ["hallway", "passage"],
-  passage: ["alter", "secretRoom", "library"],
-  alter: ["passage"],
+  passage: ["alterRoom", "secretRoom", "library"],
+  alterRoom: ["passage"],
   secretRoom: ["passage"],
-  mural: ["hallway"],
+  muralRoom: ["hallway"],
   stairs: ["hallway", "grandHall"],
   grandHall: ["stairs", "whiteDoor", "redDoor", "yellowDoor", "blackDoor", "largeDoor"],
   whiteDoor: ["grandHall"],
@@ -229,7 +229,7 @@ async function start() {
 
         // displays all posible commands
       } else if (playerInput.includes("help")) {
-        console.log("list of commands:\n'exit - quits game'\n'help - displays all commands'\n'move (location) - moves to specified location if able'\n'search - gives more detailed discriptions of certian rooms'\n'grab (item) - puts (item) in inventory'\n'inventory - displays all items in inventory'\n'use (item) (object) - uses an item on a specific object\n\nLocations and interactibles will have ' ' around them\n");
+        console.log("list of commands:\n'exit - quits game'\n'help - displays all commands'\n'move (location) - moves to specified location if able'\n'search - gives more detailed discriptions of certian rooms'\n'grab (item) - puts (item) in inventory'\n'inventory - displays all items in inventory'\n'use (item) (object) - uses an item on a specific object\n\nLocations and interactibles will have ' ' around them\ndrop (item) - drops an item in that room\nfight - does as it says");
 
         // gives more discription to the room posibly unveiling secrets
       } else if (playerInput.includes("search")){
@@ -340,7 +340,7 @@ async function start() {
           if (phylacteryBroken == true) {
             console.log(`You charge at Ruthom confident you will win. You found his phylactery and broke it rending him mortal once more. The fight lasts but a moment, he had not expected you to succeed where so many had failed. You swiftly run him through with your sword ending his unlife once and for all. The day is save, You have won.\n`);
           } else {
-            console.log(`You charge at Ruthom confident you will win. You run him through with you sword but Ruthom is unfazed. He chuckles. "Ahh, another fool follows the path of the hero. A shame." He waves his hand and you feel your life ebb from your body. All goes dark. You have died and failed in your mission.\n`);
+            console.log(`You charge at Ruthom confident you will win. You run him through with your sword but Ruthom is unfazed. He chuckles. "Ahh, another fool follows the path of the hero. A shame." He waves his hand and you feel your life ebb from your body. All goes dark. You have died and failed in your mission.\n`);
           }
           console.log("GAME OVER");
           process.exit();
@@ -360,7 +360,7 @@ async function start() {
       }
 
       // checks if pedestal puzzle is complete
-      if (squarePedestal.Pedestalinventory.includes("candle") && circlePedestal.Pedestalinventory.includes("blackHammer") && trianglePedestal.Pedestalinventory.includes("goldHeart") && hexagonPedestal.Pedestalinventory.includes("emptyJar")) {
+      if (squarePedestal.Pedestalinventory.includes("candle") && circlePedestal.Pedestalinventory.includes("blackHammer") && trianglePedestal.Pedestalinventory.includes("goldHeart") && hexagonPedestal.Pedestalinventory.includes("emptyJar") && largeDoorUnlocked == false) {
         largeDoorUnlocked = true;
         console.log(`As the last of the items are put in place the 'largeDoor' begins to shift opening the way.\n`)
       }
